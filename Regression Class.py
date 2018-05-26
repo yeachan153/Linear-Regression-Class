5991c6a60f1f151527d964b4322aa73c3ed7d164
yeachan153 <yeachan153@gmail.com>
2018-05-26 12:49:21 +0200

    Updated functions to be created

---
+++ Regression Class.py
@@ -0,0 +1,29 @@
+'''
+Dependencies
+'''
+import pandas as pd
+import numpy as np
+
+'''
+Creating the regression class
+'''
+class LinearRegression(object):
+    # Descriptives
+    def __init__(self, data):
+        self.descriptives = data.describe
+    def cost_function(self):
+        pass
+    def gradient_descent(self):
+        pass
+    def normalise(self):
+        pass
+    def train(self):
+        pass
+    def predict(self):
+        pass
+
+
+
+
+
+
